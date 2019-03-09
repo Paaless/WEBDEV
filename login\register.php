05d26151a99a3c87a703fdce7c18393bd926da27
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-09 17:50:01 +0200

    Create login\register.php

---
+++ login\register.php
@@ -0,0 +1,103 @@
+<?php
+require 'db.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+</style>
+
+</head>
+    <?php
+    if($_SERVER['REQUEST_METHOD']== 'POST')
+    {
+        if(isset($_POST['login']))
+        {
+            require 'login.php';
+        }
+        elseif(isset($_POST['register']))
+        {
+        require 'register.php';    
+        }
+    }
+?>
+<div class="form">
+      
+      <ul class="tab-group">
+        <li class="tab active"><a href="register.php">Sign Up</a></li>
+        <li class="tab"><a href="login.php">Log In</a></li>
+      </ul>
+      
+      <div class="tab-content">
+        <div id="signup">   
+          <h1>Sign Up for Free</h1>
+          
+          <form action="register.php" method="post">
+          
+          <div class="top-row">
+            <div class="field-wrap">
+              <label>
+                First Name<span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off" />
+            </div>
+        
+            <div class="field-wrap">
+              <label>
+                Last Name<span class="req">*</span>
+              </label>
+              <input type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Set A Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
+          </div>
+          
+          <button type="submit" class="button button-block">Get Started</button>
+          
+          </form>
+
+        </div>
+        
+        <div id="login">   
+          <h1>Welcome Back!</h1>
+          
+          <form action="login.php" method="post">
+          
+            <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
+          </div>
+          
+          <p class="forgot"><a href="#">Forgot Password?</a></p>
+          
+          <button class="button button-block">Log In</button>
+          
+          </form>
+
+        </div>
+        
+      </div>
+      
+    </div>
+</html>
