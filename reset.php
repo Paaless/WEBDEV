ad497e636938417cc365524c99a2834afc34188b
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-21 12:10:22 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -2,4 +2,33 @@
 require 'db.php';
 session_start();
 
+if(isset($_GET['email']) $$ !empty($_GET['email']) AND isset($_GET['hash'] $$ !empty($_GET['hash']) )
+   {
+     $email=$mysqli->escape_string($_GET['email']);
+     $hash=$mysqli->escape_string($_GET['hash']);
+     $result=$mysqli->query("SELECT * FROM users WHERE email='$email' AND hash='$hash'");
+    if($result->num_rows== 0)
+    { 
+    $_SESSION['message'] ='Link-ul accesat a intampinat o eroare!';
+      header("location: error.php");
+    }
+   }
+   else
+   {$_SESSION['message']="Verificarea a intampinat o eroare!";
+    header("location:error.php");}
 ?>
+<!DOCTYPE HTML>
+<html>
+<body>
+  <div class="form">
+    <form action="resetpass.php" method="post">
+      <div>
+        <label>Noua parola</label>
+        <input type="password" required name="newpassword" autocomplete="off">
+      </div>
+      <div>
+        <label>Confirma parola</label>
+        <input type="password" required name="confirmpassowrd" autocomplete="off">
+      </div>
+    </form>
+</html>
