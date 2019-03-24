a96a6f46883dfb3e846e0a66cae3b1b6b30c6f63
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-24 18:56:21 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,8 +4,8 @@ $target_dir="uploads/";
 $target_file=$target_dir . basename($_FILES["fileImg"]["name"]);
 $uploadOk=1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-if(isset($_POST["submit"])) 
-{echo 1;
+if($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
     $image_name=$_POST["img_name"];
     $check = getimagesize($_FILES["fileImg"]["tmp_name"]);
     if($check !== false) 
@@ -23,8 +23,8 @@ if (file_exists("$idUp"."$imageFileType")) {
     $uploadOk = 0;
 }
 else
-{ 
-$sql="INSERT INTO files (name,type,id)"."VALUES('$image_name','$imageFileType','$idUp')";}
+{
+$sql="INSERT INTO files (name,type)"."VALUES('$image_name','$imageFileType')";}
 if ($_FILES["fileImg"]["size"] > 500000) {
     echo "Marimea fisierului este prea mare.";
     $uploadOk = 0;
@@ -44,10 +44,10 @@ if ($uploadOk == 0) {
     echo "Fisierul nu a fost incarcat";
     } else 
 {   if($mysqli->query($sql))
-        {$idUp=$idUp+1;
+        {
     if (move_uploaded_file($_FILES["fileImg"]["tmp_name"], $target_file)) 
             
-        echo "Fisierul". basename( $_FILES["fileImg"]["name"]). " a fost incarcat.";
+        echo "Fisierul". basename( $_FILES["fileImg"]["name"]). " a fost incarcat cu numele $image_name .";
         else
         echo "A aparut o eroare la incarcarea fisierului.";
      } 
