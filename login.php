e3277bc27966b28278fa69e2f079a92e6b5b0877
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-26 17:47:04 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,30 @@
 <?php
-$email = $mysqli->escape_string($_POST['email']);
-$result = $mysqli->query("SELECT *FROM users WHERE email='$email'");
+require "db.php";
+$email= $mysqli->escape_string($_POST['email']);
+$result= $mysqli->query("SELECT *FROM users WHERE email='$email'");
 if($result->num_rows == 0)
 {
-$SESSION['message']="User with that email doesn't exist!";
+$SESSION['message']="Nu exista niciun utilizator cu acel email";
 header("location:error.php");
 }
 else {
 $user=$result->fetch_assoc();
 if(password_verify($_POST['password'], $user['password']) ){
 $_SESSION['email']=$user['email'];
-$_SESSION['firstname']=$user[firstname];
-$_SESSION['lastname']=$user[lastname];
-$_SESSION['active']=$user[active];
+$_SESSION['firstname']=$user['firstname'];
+$_SESSION['lastname']=$user['lastname'];
+$_SESSION['active']=$user['active'];
 $_SESSION['loggedin']=true;
-header("location:profile.php");
+header("location:index.html");
 }
 else{
-$_SESSION['message']="You have entered wrong password, try again!"
+$_SESSION['message']="Ai introdus o parola gresita!";
+
+    }
 }
+if (match_found_in_database()) {
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username; 
 }
+
 ?>
