6820427e45f32c4a64f58303143153ae6913db20
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-28 17:28:41 +0200

    Update success.php

--- success.php
+++ success.php
@@ -4,20 +4,42 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+        .bckgr{
+            background-image: url(bg.png);
+        }
+                 #buttongr{
+       text-align: center;
+        background-color:rgba(247, 247, 255, 1);
+        text-decoration: none;
+    }
+    
+    .button{
+         border: none;
+        padding: 15px 32px;
+          float: inherit;
+        text-decoration: none;
+        background-color:rgba(247, 247, 255, 1);
+        font-size: 15;
+        font-family: fantasy;
+    }
+     .button:hover {
+  background-color:rgba(189, 213, 234, 1);
+}
+    </style>
 </head>
-<body>
+<body class="bckgr">
 <div class="form">
-<h1><?= 'Success';?></h1>
+<h1><?= 'Succes';?></h1>
 <p>
 <?php
 if( isset($_SESSION['message']) AND !empty($_SESSION['message']))
 echo $_SESSION['message'];
 else
 header("location:index.html");
-endif;
 ?>
 </p>
-<a href="index.html"><button class="home-btn">Acasa</button></a>
+    <button class="button"><a href="index.html">Acasa</a></button>
     </div>
     </body>
 </html>
