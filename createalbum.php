9305b599cbb7900f2bb065bb7739e67e2a130ac2
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-29 21:38:39 +0200

    Update createalbum.php

--- createalbum.php
+++ createalbum.php
@@ -1,8 +1,11 @@
 <?php
 require "file_db.php";
+$newcontent = file_get_contents("template.html");
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
  $album_name=$_POST["alb_name"];
+ $album_folder_name=$_POST['alb_name'];
 $result = $mysqli->query("SELECT * FROM albums WHERE name ='$album_name' ") or die($mysqli->error());
 
     if($result->num_rows > 0)
@@ -10,14 +13,21 @@ $result = $mysqli->query("SELECT * FROM albums WHERE name ='$album_name' ") or d
     $_SESSION['message']='Exista deja un album cu acest nume!';
 }
     else
-    {
-       $sql="INSERT INTO albums (name,user_id)"."VALUES('$album_name','1')";
+    {  $sql="INSERT INTO albums (name,user_id)"."VALUES('$album_name','1')";
  if($mysqli->query($sql))
- {
-     echo 1;
+ { if(mkdir("$album_name"));
+  { $handle = fopen("$album_folder_name/$album_name.html",'w+');
+   fwrite($handle,$newcontent); 
+  fclose($handle);}
      $_SESSION['message']="A fost creat albumul $album_name .";
+  header('location:albume.html');
  }
     }
 }
-echo $_SESSION['message'];
+echo $_SESSION['message'];}
+else
+{
+    $_SESSION['messagelog']="Trebuie sa te conectezi pentru a putea incarca fisiere!";
+    header("location:testphp.php");
+}
 ?>
