15d30b92d86cd90a6e607c7bc433c7a67260f1e4
Paaless <45945412+Paaless@users.noreply.github.com>
2019-03-29 23:22:58 +0200

    Update login\register.php

--- login\register.php
+++ login\register.php
@@ -1,16 +1,40 @@
-<?php
-require 'file_db.php';
-?>
 <!DOCTYPE html>
 <html>
 <head>
 <style>
     .bckgr{background-image:url(bg.png);}
+    .tab active{
+     text-align: center;
+    }
+    .tab{
+     text-align: center;
+    }
+    .field-wrap{
+     text-align: center;
+    }
+    .button{
+         border: none;
+        padding: 15px 32px;
+          float: inherit;
+        text-decoration: none;
+        background-color:rgba(247, 247, 255, 1);
+        font-size: 15;
+        font-family: fantasy;
+        position: absolute;
+
+    }
+    .wrapper{
+        text-align: center;
+    }
+     .button:hover {
+  background-color:rgba(189, 213, 234, 1);
+}
+
 </style>
 
 </head>
     <body class="bckgr">
-    <?php
+<?php
     if($_SERVER['REQUEST_METHOD']== 'POST')
     {
         if(isset($_POST['login']))
@@ -24,35 +48,25 @@ require 'file_db.php';
     }
 ?>
     <div class="message" style="float:center;">
-        <?php
-        if($_SESSION['messagelog']&& $_SESSION['messagelog']!="")
-        echo $_SESSION['messagelog'];
-        ?>
     </div>
-<div class="form">
-      
-      <ul class="tab-group">
-        <li class="tab active"><a href="register.php">Sign Up</a></li>
-        <li class="tab"><a href="login.php">Log In</a></li>
-      </ul>
-      
+<div class="form"> 
       <div class="tab-content">
         <div id="signup">   
-          <h1>Sign Up for Free</h1>
+          <h1 style="color:white; text-align: center;">Register</h1>
           
-          <form action="register.php" method="post">
+          <form action="register1.php" method="post">
           
           <div class="top-row">
             <div class="field-wrap">
               <label>
-                First Name<span class="req">*</span>
+                First Name<span class="req">* &nbsp; &nbsp; &nbsp; &nbsp;</span>
               </label>
               <input type="text" required autocomplete="off" name="firstname" />
             </div>
         
             <div class="field-wrap">
               <label>
-                Last Name<span class="req">*</span>
+                Last Name<span class="req">* &nbsp; &nbsp; &nbsp; &nbsp;</span>
               </label>
               <input type="text"required autocomplete="off" name="lastname"/>
             </div>
@@ -60,7 +74,7 @@ require 'file_db.php';
 
           <div class="field-wrap">
             <label>
-              Email Address<span class="req">*</span>
+              Email Address<span class="req">* &nbsp;</span>
             </label>
             <input type="email"required autocomplete="off" name="email"/>
           </div>
@@ -69,38 +83,39 @@ require 'file_db.php';
             <label>
               Set A Password<span class="req">*</span>
             </label>
-            <input type="password"required autocomplete="off"/>
+            <input type="password"required autocomplete="off" name="password"/>
           </div>
-          
-          <button type="submit" class="button button-block">Get Started</button>
+          <div class="wrapper" style="margin-bottom:50px;">
+          <br><button type="submit" class="button">Get Started</button>
+         </div>
           
           </form>
 
         </div>
         
         <div id="login">   
-          <h1>Welcome Back!</h1>
+          <h1 style="color: white; text-align: center;">Welcome Back!</h1>
           
-          <form action="login.php" method="post">
+          <form action="login1.php" method="post">
           
             <div class="field-wrap">
             <label>
               Email Address<span class="req">*</span>
             </label>
-            <input type="email"required autocomplete="off"/>
+            <input type="email"required autocomplete="off" name="email"/>
           </div>
           
           <div class="field-wrap">
             <label>
-              Password<span class="req">*</span>
+              Password<span class="req">* &nbsp; &nbsp; &nbsp; &nbsp;</span>
             </label>
-            <input type="password"required autocomplete="off"/>
+            <input type="password"required autocomplete="off" name="password"/>
           </div>
           
-          <p class="forgot"><a href="forgot.html">Forgot Password?</a></p>
-          
-          <button type="submit" class="button button-block">Log In</button>
-          
+          <p class="forgot" style="text-align: center;"><a href="forgot.html" style="text-decoration: none;text-decoration-color: aliceblue;">Forgot Password?</a></p>
+          <div class="wrapper">
+              <button type="submit" class="button">Log In</button>
+          </div>
           </form>
 
         </div>
